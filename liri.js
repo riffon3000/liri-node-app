ec3ede76b7126a38f7e4d09598bb16693af06ba0
riffon3000 <riffon@gmail.com>
2018-10-19 14:11:43 -0500

    add Liri prompt with switch case

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require('dotenv').config();
 const keys = require('./keys');
 const inquirer = require('inquirer');
 const request = require('request');
@@ -7,3 +7,42 @@ const moment = require('moment');
 const spotifyKey = new Spotify(keys.spotify);
 const ombdKey = new OBDb(keys.ombd);
 const bandsintownKey = new Bandsintown(keys.bandsintown);
+
+function liri() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Hi, I\'m Liri! What would you like to do?',
+                choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-says'],
+                name: 'command'
+            },
+            {
+                type: 'confirm',
+                message: 'Are you sure?',
+                name: 'confirm',
+                default: true
+            }
+        ])
+        .then(function (commandChoice) {
+            if (commandChoice.confirm) {
+                switch (commandChoice.command) {
+                    case 'concert-this':
+                        searchBands();
+                        break;
+                    case 'spotify-this-song':
+                        searchSpotify();
+                        break;
+                    case 'movie-this':
+                        searchOMDb();
+                        break;
+                    case 'do-what-it-says':
+                        readRandomTxt();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+};
+
