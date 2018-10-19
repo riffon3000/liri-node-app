e0042bbba33b53c55d6bbdd660a71dbdd5036be0
riffon3000 <riffon@gmail.com>
2018-10-19 12:46:56 -0500

    update keys

--- liri.js
+++ liri.js
@@ -1,12 +1,2 @@
-// $ npm install --save node-spotify-api
-// + node-spotify-api@1.0.7
-// added 53 packages in 38.525s
-// $ npm install moment --save
-// + moment@2.22.2
-// added 1 package in 13.171s
-// $ npm install dotenv
-// + dotenv@6.1.0
-// added 1 package in 33.457s
-
 require("dotenv").config();
 var spotify = new Spotify(keys.spotify);
